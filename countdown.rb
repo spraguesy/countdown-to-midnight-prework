f91affdcae96f8a142a23adce70b9af9911ef6c8
Andrew Sprague <andrewsprague@outlook.com>
2018-08-12 00:17:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -6,10 +6,11 @@ def countdown (number)
     puts "#{counter} SECOND(S)!"
     counter -= 1
   end
-  puts "HAPPY NEW YEAR!"  
-  
+  puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
+def countdown_with_sleep (number)
+  
+  
   
 end
