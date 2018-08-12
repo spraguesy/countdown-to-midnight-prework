8eea735c0c25d5b4dbc44ce30236873bff413eae
Andrew Sprague <andrewsprague@outlook.com>
2018-08-12 00:19:54 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -5,13 +5,18 @@ def countdown (number)
   while counter > 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
-    sleep(1)
   end
   puts "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep (number)
-  
+  counter = number
+  while counter > 0
+    puts "#{counter} SECOND(S)!"
+    counter -= 1
+    sleep(1)
+  end
+  puts "HAPPY NEW YEAR!"
   
   
 end
